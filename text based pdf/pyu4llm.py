1f86fe189f9a284ec59bffe9a82d20b3fc94f403
jo-s-eph <maouchejoseph@gmail.com>
2025-02-12 09:15:54 +0100

    clean up pour la solution text based

---
+++ text based pdf/pyu4llm.py
@@ -0,0 +1,29 @@
+import pymupdf4llm
+import pymupdf
+import pprint
+import json
+import sys
+
+#handle Rect objects
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, pymupdf.Rect):
+            return [obj.x0, obj.y0, obj.x1, obj.y1]
+        return super().default(obj)
+
+def process_pdf(input_file):
+    md_text = pymupdf4llm.to_markdown(input_file, write_images=True, image_path="output", page_chunks=True)
+    for i, item in enumerate(md_text):
+        json_str = json.dumps(item, cls=CustomJSONEncoder, indent=2)
+
+        with open(f'output/output_page_{i+1}.json', 'w+') as f:
+            f.write(json_str)
+        print(f"Saved output_page_{i+1}.json")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <pdf_file_path>")
+        sys.exit(1)
+    
+    input_file = sys.argv[1]
+    process_pdf(input_file)
